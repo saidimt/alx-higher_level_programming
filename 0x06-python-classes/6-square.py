a461a03942dd7f46a925a1400b02f416823a0545
mtembezi <85906757+saidimt@users.noreply.github.com>
2022-09-14 02:33:38 +0300

    that defines a square by: (based on 5-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,9 +5,7 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0, position=(0, 0)):
-        """Initialize a new square.
-        Args:
+    def __init__(sel
             size (int): The size of the new square.
             position (int, int): The position of the new square.
         """

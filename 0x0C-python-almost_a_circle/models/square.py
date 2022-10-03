77c17a1364affb3ca1ee23b13f79abb72fcd75a9
mtembezi <85906757+saidimt@users.noreply.github.com>
2022-10-03 23:05:01 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,53 +1,56 @@
 #!/usr/bin/python3
-""" Model Square that inherits from Base """
-
+"""Creating a square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ Class that defines a Square Object """
-
+    """Defining square class"""
     def __init__(self, size, x=0, y=0, id=None):
-        """ Constructor """
+        """Initializing square class"""
         super().__init__(size, size, x, y, id)
-        
+
+    def __str__(self):
+        """Printing square class by overloading __str__ method"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
+
     @property
     def size(self):
-        """ size getter """
+        """Getting size of square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """ size setter """
+        """Setting size of square"""
         self.width = value
         self.height = value
-        
-    def __str__(self):
-        """ Print Method """
-        st = "[Square] ({:d}) {:d}/{:d} - {:d}"
-        st = st.format(self.id, self.x, self.y, self.width)
-        return st
-    
-    def update(self, *args, **kwargs):
-        """ Functions to update arguments of each attr """
-        arlist = ["id", "size", "x", "y"]
-        ct = 0
-        if args and len(args) != 0:
-            for ar in args:
-                if ct == 0:
-                    super().update(ar)
-                elif ct < len(arlist):
-                    setattr(self, arlist[ct], ar)
-                ct += 1
 
+    def update(self, *args, **kwargs):
+        """Updating square class"""
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
         else:
             for key, value in kwargs.items():
                 if key == "id":
-                    super().update(id=value)
-                else:
-                    setattr(self, key, value)
-                    
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
     def to_dictionary(self):
-        """ Returns dicitionary representation of Square """
-        dic = {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
-        return dic
+        """Returning dictionary representation of square"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
